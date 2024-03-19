74c55ad76abbfdf136b05ca6a96c4c31c8db1f95
Ethan Jewell <ethanjevvell@gmail.com>
2024-03-19 11:28:09 -0500

    Reorganize helios slightly

--- Scripts/STU Helios Node/Program.cs
+++ Scripts/STU Helios Node/Program.cs
@@ -7,7 +7,7 @@ using VRage.Game.ModAPI.Ingame;
 namespace IngameScript {
     partial class Program : MyGridProgram {
 
-        string slaveNodeName;
+        string nodeName;
 
         STUMasterLogBroadcaster masterLogBroadcaster;
         STULog outgoingLog = new STULog();
@@ -27,7 +27,7 @@ namespace IngameScript {
             GridTerminalSystem.GetBlocksOfType<IMySolarPanel>(solarPanels);
             GridTerminalSystem.GetBlocksOfType<IMyCargoContainer>(storageBlocks);
 
-            slaveNodeName = Me.CustomData;
+            nodeName = Me.CustomData;
 
             if (solarPanels == null) {
                 throw new Exception("No solar panels found");
@@ -37,15 +37,48 @@ namespace IngameScript {
                 throw new Exception("No storage blocks found");
             }
 
-            if (string.IsNullOrEmpty(slaveNodeName)) {
+            if (string.IsNullOrEmpty(nodeName)) {
                 throw new Exception("No slave node name found");
             }
+
         }
 
         public void Main() {
 
             solarPanelOutput = 0;
             gatlingAmmoBoxes = 0;
+
+            InspectSolarPanels();
+            InspectInventories();
+
+            Echo("Total solar panel output: " + solarPanelOutput + " MW");
+            Echo("Number of broken panels: " + brokenPanels.Count);
+            Echo("Number of gatling ammo boxes: " + gatlingAmmoBoxes);
+
+            if (brokenPanels.Count > 0) {
+                Echo("WARNING: Broken panel(s) detected");
+                outgoingLog.Sender = nodeName;
+                outgoingLog.Message = "Broken solar panels detected: " + brokenPanels.Count;
+                outgoingLog.Type = STULogType.WARNING;
+                masterLogBroadcaster.Log(outgoingLog);
+            }
+
+        }
+
+        public void InspectInventories() {
+            foreach (IMyCargoContainer storage in storageBlocks) {
+                List<MyInventoryItem> items = new List<MyInventoryItem>();
+                storage.GetInventory().GetItems(items);
+
+                foreach (MyInventoryItem item in items) {
+                    if (item.Type.SubtypeId.ToString() == "NATO_25x184mm") {
+                        gatlingAmmoBoxes += item.Amount.ToIntSafe();
+                    }
+                }
+            }
+        }
+
+        public void InspectSolarPanels() {
 
             foreach (IMySolarPanel panel in solarPanels) {
 
@@ -58,29 +91,7 @@ namespace IngameScript {
                 solarPanelOutput += panel.CurrentOutput;
 
             }
-
-            foreach (IMyCargoContainer storage in storageBlocks) {
-                List<MyInventoryItem> items = new List<MyInventoryItem>();
-                storage.GetInventory().GetItems(items);
-
-                foreach (MyInventoryItem item in items) {
-                    if (item.Type.SubtypeId.ToString() == "NATO_25x184mm") {
-                        gatlingAmmoBoxes += item.Amount.ToIntSafe();
-                    }
-                }
-            }
-
-            Echo("Total solar panel output: " + solarPanelOutput + " MW");
-            Echo("Number of broken panels: " + brokenPanels.Count);
-            Echo("Number of gatling ammo boxes: " + gatlingAmmoBoxes);
-
-            if (brokenPanels.Count > 0) {
-                Echo("WARNING: Broken panel(s) detected");
-                outgoingLog.Sender = slaveNodeName;
-                outgoingLog.Message = "Broken solar panels detected: " + brokenPanels.Count;
-                outgoingLog.Type = STULogType.WARNING;
-                masterLogBroadcaster.Log(outgoingLog);
-            }
         }
+
     }
 }
